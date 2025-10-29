8e42abfecee2d4bd8154ebf16af221feddded64f
TiagoCarvalho-true <carvalhost58@gmail.com>
2025-10-29 17:59:00 -0400

    add structure of modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PackagesModule } from './packages/packages.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PackagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
